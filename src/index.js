e4039d5f2e1bcf7bd62836c35590149d84f49602
maryna-maksimenka <maksimenka.maryna@gmail.com>
2020-03-22 12:45:10 +0300

    feat: implement the decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    let start = 0;
+
+    for (let i = 0; i < expr.length / 10; i++) {
+        let symbol = expr.slice(start, start + 10);
+        start += 10;
+        if (symbol == '**********') {
+            decoded += ' ';
+            continue;
+        }
+
+        let encoded = '';
+        for (let j = 10; j > 0; j -= 2) {
+            switch (symbol.slice(j - 2, j)) {
+                case '10':
+                    encoded += '.';
+                    break;
+                case '11':
+                    encoded += '-';
+                    break;
+            }
+        }
+
+        decoded += MORSE_TABLE[encoded.split('').reverse().join('')];    
+    }
+
+    return decoded;
 }
 
 module.exports = {
